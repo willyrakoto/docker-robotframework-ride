eea364ceb7a74d28a1359590c91c9ee2f623ce93
willyrakoto <willy.rakotoarisoa@gmail.com>
2017-01-10 11:17:20 +0100

    Update runpybot.sh

--- runpybot.sh
+++ runpybot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pybot --argumentfile /tmp/RIDE7BUd9y.d/argfile.txt --listener /usr/local/lib/python2.7/dist-packages/robotide/contrib/testrunner/TestRunnerAgent.py:33218:False /robot
+pybot /robot/Get_user_home.txt
